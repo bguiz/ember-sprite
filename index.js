24b8f7f54ecd120084fe004fa6e1dfd7014cff67
bguiz <brendan.graetz@gmail.com>
2014-12-10 23:16:30 +1100

    =BG= spike feature for svg spriting via parallel spike in broccoli-sprite

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var util = require('util');
 var brocMergeTrees = require('broccoli-merge-trees');
 var brocSprite = require('broccoli-sprite');
+var brocVectorSprite = require('broccoli-sprite/vector');
 var brocConcat = require('broccoli-concat');
 var brocDelete = require('broccoli-file-remover');
 var brocPickFiles = require('broccoli-static-compiler');
@@ -19,23 +20,32 @@ function postprocessTree(type, workingTree) {
             files: this.app.options.sprite.src,
             destDir: '/',
         });
+        var vectorSpriteTree = brocPickFiles(workingTree,  {
+            srcDir: '/',
+            files: this.app.options.vectorSprite.src,
+            destDir: '/',
+        });
         if (!!this.app.options.sprite.debug) {
             console.log('spriteTree', util.inspect(workingTree, false, 6, true));
         }
         spriteTree = brocSprite(spriteTree, this.app.options.sprite);
+        vectorSpriteTree = brocVectorSprite(vectorSpriteTree, this.app.options.vectorSprite);
         workingTree = brocMergeTrees([
             workingTree,
-            spriteTree
+            spriteTree,
+            vectorSpriteTree
         ]);
 
         //sprites.css is appended to app.css,
         //so that two separate styles sheets do not need to get linked from index.html
         var appCssFile = 'assets/' + (this.app.name || this.app.project.pkg.name) + '.css';
         var spriteCssFile = this.app.options.sprite.stylesheetPath;
+        var vectorSpriteCssFile = this.app.options.vectorSprite.stylesheetPath;
         var treeConcatCss = brocConcat(workingTree,  {
             inputFiles: [
                 appCssFile,
-                spriteCssFile
+                spriteCssFile,
+                vectorSpriteCssFile
             ],
             outputFile: '/'+appCssFile,
             wrapInFunction: false,
