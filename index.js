00e45d555849c110225d32f31509467d1cee0383
Brian Runnells <brunnells@heroku.com>
2015-06-29 15:17:17 -0700

    Enable multiple sprites to be passed in

--- index.js
+++ index.js
@@ -7,51 +7,74 @@ var brocConcat = require('broccoli-concat');
 var brocDelete = require('broccoli-file-remover');
 var brocPickFiles = require('broccoli-static-compiler');
 
-module.exports.name = 'ember-sprite';
+module.exports = {
+  name: 'ember-sprite',
 
-module.exports.treeFor = function treeFor( /*inTree*/ ) {};
+  treeFor: function treeFor( /*inTree*/ ) {},
 
-module.exports.postprocessTree =
-function postprocessTree(type, workingTree) {
-    if (type === 'all') {
-        var spriteTree = brocPickFiles(workingTree, {
-            srcDir: '/',
-            files: this.app.options.sprite.src,
-            destDir: '/',
-        });
-        if (!!this.app.options.sprite.debug) {
-            console.log('spriteTree', util.inspect(workingTree, false, 6, true));
+  postprocessTree: function postprocessTree(type, workingTree) {
+      if (type === 'all' && this.app.options.sprite) {
+        var self = this;
+
+        // for backwards compatibility to previous implementation that passed plain object into sprite
+        // we push that object into an array we can iterate through to process the sprite(s)
+        if(Object.prototype.toString.call(this.app.options.sprite) === '[object Object]') {
+          var tmp = this.app.options.sprite;
+          this.app.options.sprite = [];
+          this.app.options.sprite.push(tmp);
         }
-        spriteTree = brocSprite(spriteTree, this.app.options.sprite);
-        workingTree = brocMergeTrees([
-            workingTree,
-            spriteTree
-        ]);
 
-        //sprites.css is appended to app.css,
-        //so that two separate styles sheets do not need to get linked from index.html
-        var appCssFile = 'assets/' + (this.app.name || this.app.project.pkg.name) + '.css';
-        var spriteCssFile = this.app.options.sprite.stylesheetPath;
-        var treeConcatCss = brocConcat(workingTree,  {
-            inputFiles: [
-                appCssFile,
-                spriteCssFile
-            ],
-            outputFile: '/'+appCssFile,
-            wrapInFunction: false,
+        // process each of the sprites that was passed in
+        this.app.options.sprite.forEach(function(sprite) {
+          workingTree = self._processSprite(sprite, workingTree);
         });
-        workingTree = brocMergeTrees([
-            workingTree,
-            treeConcatCss
-        ], {
-            overwrite: true,
-        });
-        workingTree = brocDelete(workingTree, {
-            files: [
-                spriteCssFile
-            ],
-        });
+      }
+
+      return workingTree;
+  },
+
+  _processSprite: function(sprite, workingTree) {
+
+    var spriteTree = brocPickFiles(workingTree, {
+        srcDir: '/',
+        files: sprite.src,
+        destDir: '/',
+    });
+
+    if (!!sprite.debug) {
+        console.log('spriteTree', util.inspect(workingTree, false, 6, true));
     }
+    spriteTree = brocSprite(spriteTree, sprite);
+    workingTree = brocMergeTrees([
+        workingTree,
+        spriteTree
+    ]);
+
+    //sprites.css is appended to app.css,
+    //so that two separate styles sheets do not need to get linked from index.html
+    var appCssFile = 'assets/' + (this.app.name || this.app.project.pkg.name) + '.css';
+    var spriteCssFile = sprite.stylesheetPath;
+    var treeConcatCss = brocConcat(workingTree,  {
+        inputFiles: [
+            appCssFile,
+            spriteCssFile
+        ],
+        outputFile: '/'+appCssFile,
+        wrapInFunction: false,
+    });
+
+    workingTree = brocMergeTrees([
+        workingTree,
+        treeConcatCss
+    ], {
+        overwrite: true,
+    });
+    workingTree = brocDelete(workingTree, {
+        files: [
+            spriteCssFile
+        ],
+    });
 
     return workingTree;
+  }
 };
